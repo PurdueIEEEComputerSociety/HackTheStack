fcc2803641318ce430f53f1ea82e2c64b074d5ba
Bobby Zhang <zhan2175@purdue.edu>
2016-12-03 00:22:29 -0500

    Simplify checking confliction procedure

--- 0-password-list/Hash.java
+++ 0-password-list/Hash.java
@@ -2,11 +2,14 @@
  * Created by Bobby on 12/2/2016.
  */
 import java.io.*;
+import java.util.HashSet;
 
 public class Hash {
     public static void main(String[] args) throws IOException{
         BufferedReader in = null;
         BufferedWriter out = null;
+        HashSet<String> set = new HashSet<>();
+        int count = 0;
 
         try {
             in = new BufferedReader(new FileReader(new File("CommonPassword")));
@@ -14,7 +17,16 @@ public class Hash {
 
             String str;
             while ((str = in.readLine()) != null) {
-                out.write(Hashing.hashing(str) + "\n");
+                count++;
+                String hashed = Hashing.hashing(str);
+                out.write(hashed + "\n");
+                if (!set.contains(str)) {
+                    set.add(str);
+                }
+            }
+
+            if (set.toArray().length == count) {
+                System.out.println("No confliction.");
             }
         } finally {
             if (in != null) {
