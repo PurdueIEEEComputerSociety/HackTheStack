7e762aac44a8b4356f93bababf0c30d7e6653e31
raghav198 <malikraghav9@gmail.com>
2016-12-06 03:04:21 +0000

    Source code for buffer overflow attack
    This is just source code that needs to be included. There also has to be a compiled binary thats set to execute as root

---
+++ 2-buffer-overflow/buffer.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void inner_function(int);
+void outer_function();
+void unsafe_function();
+char* argument;
+char* buffer;
+void inner_function(int i)
+{
+	printf("%d %p\n", i, unsafe_function);
+	char local_buffer[8] = "bbbbbbb\0";
+	char buf[8] = "aaaaaaa\0";
+	//scanf("%s", buf);
+	strcpy(buf, buffer);
+	printf("Value of buf: %s\n", buf);
+	printf("Value of local buffer: %s\n", local_buffer);
+	argument = (char*)local_buffer;
+}
+
+void outer_function()
+{
+	int x = 117, i = 0;
+	while (x > 1)
+	{
+		if (x % 2) x = x * 3 + 1;
+		else x = x / 2;
+		i++;
+	}
+	inner_function(i);
+}
+void unsafe_function()
+{
+	printf("+========+\n|        |\n| HACKED |\n|        |\n+========+\n\n");
+	//Replace '\xff' with a null byte
+	for (int i = 0; i < 256; i++)
+	{
+		//Handle two's complement :)
+		if (argument[i] == -1 || argument[i] == 255)
+		{
+			argument[i] = 0;
+			break;
+		}
+	}
+	printf("------executing %s-----\n", argument);
+	system(argument);
+}
+int main(int argc, char** argv)
+{
+	buffer = argv[1];
+	outer_function();
+	return 0;
+}
